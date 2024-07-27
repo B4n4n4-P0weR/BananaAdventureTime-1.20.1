ec7a0fd13f32c7991037ad3e53ca27601ef59c87
B4n4n4_P0wer <vladtv2374@yandex.ru>
2024-07-27 18:45:18 +1000

    Stacks to update for stuff items

--- src/main/java/net/B4n4n4_P0wer/banana_adventure_time/item/ModItems.java
+++ src/main/java/net/B4n4n4_P0wer/banana_adventure_time/item/ModItems.java
@@ -15,14 +15,14 @@ public class ModItems {
             () -> new Item(new Item.Properties()));
 
     public static final RegistryObject<Item> MARCELINE_AXE  = ITEMS.register("marceline_axe",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> MARCELINE_AXE_GUITAR  = ITEMS.register("marceline_axe_guitar",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
     public static final RegistryObject<Item> ICE_KING_CROWN  = ITEMS.register("ice_king_crown",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> EMPTY_ICE_KING_CROWN  = ITEMS.register("empty_ice_king_crown",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
 
     public static final RegistryObject<Item> COME_ALONG_WITH_ME  = ITEMS.register("come_along_with_me",
